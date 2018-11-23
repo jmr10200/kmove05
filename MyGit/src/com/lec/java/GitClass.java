ea2f62838e65da66f4c97d02b000c6084583ead1
jm <jm@DESKTOP-O57T4GT>
2018-11-23 11:49:58 +0900

    첫 업로드

---
+++ MyGit/src/com/lec/java/GitClass.java
@@ -0,0 +1,15 @@
+package com.lec.java;
+/**
+ * 
+ * @author jm
+ *
+ */
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GitHub 올려보기");
+	}
+
+}
